b73ed52acce156c6e8a5484a8a5b06a656197d17
rafaelcruz <rafaelcruz@192.168.0.9>
2016-10-11 14:36:30 -0400

    Ya tiene los flags y las bombas set

--- Lab5/src/MyMouseAdapter.java
+++ Lab5/src/MyMouseAdapter.java
@@ -1,68 +1,84 @@
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.Shape;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Random;
-
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class MyMouseAdapter extends MouseAdapter {
-	private Random generator = new Random();
+	
 	public void mousePressed(MouseEvent e) {
+		Component c = e.getComponent();
+		while (!(c instanceof JFrame)) {
+			c = c.getParent();
+			if (c == null) {
+				return;
+			}
+		}
+		JFrame myFrame = (JFrame) c;
+		MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);
+		Insets myInsets = myFrame.getInsets();
+		int x1 = myInsets.left;
+		int y1 = myInsets.top;
+		e.translatePoint(-x1, -y1);
+		int x = e.getX();
+		int y = e.getY();
+		myPanel.x = x;
+		myPanel.y = y;
 		switch (e.getButton()) {
 		case 1:		//Left mouse button
-			Component c = e.getComponent();
-			while (!(c instanceof JFrame)) {
-				c = c.getParent();
-				if (c == null) {
-					return;
-				}
-			}
-			JFrame myFrame = (JFrame) c;
-			MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);
-			Insets myInsets = myFrame.getInsets();
-			int x1 = myInsets.left;
-			int y1 = myInsets.top;
-			e.translatePoint(-x1, -y1);
-			int x = e.getX();
-			int y = e.getY();
-			myPanel.x = x;
-			myPanel.y = y;
+
 			myPanel.mouseDownGridX = myPanel.getGridX(x, y);
 			myPanel.mouseDownGridY = myPanel.getGridY(x, y);
 			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
 			//Do nothing
+			myPanel.mouseDownGridX = myPanel.getGridX(x, y);
+			myPanel.mouseDownGridY = myPanel.getGridY(x, y);
+			myPanel.repaint();
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
 			break;
 		}
 	}
+	
 	public void mouseReleased(MouseEvent e) {
-		switch (e.getButton()) {
-		case 1:		//Left mouse button
-			Component c = e.getComponent();
-			while (!(c instanceof JFrame)) {
-				c = c.getParent();
-				if (c == null) {
-					return;
-				}
+		Component c = e.getComponent();
+		while (!(c instanceof JFrame)) {
+			c = c.getParent();
+			if (c == null) {
+				return;
 			}
-			JFrame myFrame = (JFrame)c;
-			MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
-			Insets myInsets = myFrame.getInsets();
-			int x1 = myInsets.left;
-			int y1 = myInsets.top;
-			e.translatePoint(-x1, -y1);
-			int x = e.getX();
-			int y = e.getY();
-			myPanel.x = x;
-			myPanel.y = y;
-			int gridX = myPanel.getGridX(x, y);
-			int gridY = myPanel.getGridY(x, y);
+		}
+		JFrame myFrame = (JFrame)c;
+		MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
+		Insets myInsets = myFrame.getInsets();
+		int x1 = myInsets.left;
+		int y1 = myInsets.top;
+		e.translatePoint(-x1, -y1);
+		int x = e.getX();
+		int y = e.getY();
+		myPanel.x = x;
+		myPanel.y = y;
+		Color uncoveredGrid = Color.WHITE;
+		Color flag = Color.RED;
+		//TODO cuando se seleccione un cuadro con el valor
+		Color bomb = Color.BLACK;
+		Color coveredGrid = Color.lightGray;
+
+		int gridX = myPanel.getGridX(x, y);
+		int gridY = myPanel.getGridY(x, y);
+		
+		
+		switch (e.getButton()){
+		case 1:		//Left mouse button
+			
 			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
 				//Had pressed outside
 				//Do nothing
@@ -76,38 +92,94 @@ public class MyMouseAdapter extends MouseAdapter {
 						//Do nothing
 					} else {
 						//Released the mouse button on the same cell where it was pressed
-						if ((gridX == 0) || (gridY == 0)) {
+						if (myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] == flag) {
 							//On the left column and on the top row... do nothing
-						} else {
-							//On the grid other than on the left column and on the top row:
-							Color newColor = null;
-							switch (generator.nextInt(5)) {
-							case 0:
-								newColor = Color.YELLOW;
-								break;
-							case 1:
-								newColor = Color.MAGENTA;
-								break;
-							case 2:
-								newColor = Color.BLACK;
-								break;
-							case 3:
-								newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
-							case 4:
-								newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
-							}
-							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
+						} 
+						else {
+							myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = uncoveredGrid;
 							myPanel.repaint();
+							myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = Color.LIGHT_GRAY;
+						}
+						if((myPanel.bombs[myPanel.mouseDownGridX][myPanel.mouseDownGridY] == 1) && 
+								myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] != flag){
+							myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = bomb;
+							myPanel.repaint();
+							JOptionPane.showMessageDialog(null, "Bomb Clicked!", "Minesweeper", JOptionPane.INFORMATION_MESSAGE);
+							System.exit(0);
 						}
 					}
 				}
 			}
+			
 			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
-			//Do nothing
+//			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
+//				//Had pressed outside
+//				//Do nothing
+//			} else {
+//				if ((gridX == -1) || (gridY == -1)) {
+//					//Is releasing outside
+//					//Do nothing
+//				} else {
+//						//Released the mouse button on the same cell where it was pressed
+//						if ((myPanel.mouseDownGridX == gridX) && (myPanel.mouseDownGridY == gridY)) {
+//							//On the left column and on the top row... do nothing
+//							if(myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] == uncoveredGrid){
+//								//Do nothing
+//							}
+//	 
+//						else {
+//							if(myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] == coveredGrid){
+//								myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = flag;
+//								myPanel.repaint();
+//							}
+//							else{
+//								if(myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] == flag){
+//									myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = coveredGrid;
+//									myPanel.repaint();
+//								}
+//							}
+//						}
+//						}
+//				}
+//			}
+			//myPanel.repaint();
+			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
+				//Had pressed outside
+				//Do nothing
+			} else {
+				if ((gridX == -1) || (gridY == -1)) {
+					//Is releasing outside
+					//Do nothing
+				} else {
+					//Cuando haces click en un grid
+					if((myPanel.mouseDownGridX == gridX) && (myPanel.mouseDownGridY == gridY)){
+						//Si ese grid es color blanco no hagas nada
+						if(myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] == coveredGrid){
+							//Do nothing
+						}
+						else {  
+							//Si ese grid es color gris entonces poner el flag
+							if(myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] == uncoveredGrid){
+								myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = flag;
+								myPanel.repaint();	
+
+							}
+							else{
+								//si ese grid tiene un flag entonces puedes quitarla
+								if(myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] == flag){
+									myPanel.cells[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = uncoveredGrid;
+									myPanel.repaint();
+
+								}
+							}
+								
+						}
+					}
+				
+				}
+			}
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
--- Lab5/src/MyPanel.java
+++ Lab5/src/MyPanel.java
@@ -1,8 +1,14 @@
 import java.awt.Color;
+
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.util.Random;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
@@ -10,14 +16,25 @@ public class MyPanel extends JPanel {
 	private static final int GRID_X = 25;
 	private static final int GRID_Y = 25;
 	private static final int INNER_CELL_SIZE = 29;
-	private static final int TOTAL_COLUMNS = 10;
-	private static final int TOTAL_ROWS = 11;   //Last row has only one cell
+	private static final int TOTAL_COLUMNS = 9;
+	private static final int TOTAL_ROWS = 9;   //Last row has only one cell
+	private static final int AMOUNT_OF_MINES = 10;
 	public int x = -1;
 	public int y = -1;
 	public int mouseDownGridX = 0;
 	public int mouseDownGridY = 0;
-	public Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
+	public int amountOfNearMines = 0;
+	
+	private Random random;
+	
+	private BufferedImage image;
+	
+	public Color[][] cells = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
+	public int [][] bombs = new int [TOTAL_COLUMNS][TOTAL_ROWS];
+	public int [][] neighbors = new int[TOTAL_COLUMNS][TOTAL_ROWS];
+	
 	public MyPanel() {   //This is the constructor... this code runs first to initialize
+		
 		if (INNER_CELL_SIZE + (new Random()).nextInt(1) < 1) {	//Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("INNER_CELL_SIZE must be positive!");
 		}
@@ -27,21 +44,20 @@ public class MyPanel extends JPanel {
 		if (TOTAL_ROWS + (new Random()).nextInt(1) < 3) {	//Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("TOTAL_ROWS must be at least 3!");
 		}
-		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //Top row
-			colorArray[x][0] = Color.LIGHT_GRAY;
-		}
-		for (int y = 0; y < TOTAL_ROWS; y++) {   //Left column
-			colorArray[0][y] = Color.LIGHT_GRAY;
-		}
-		for (int x = 1; x < TOTAL_COLUMNS; x++) {   //The rest of the grid
-			for (int y = 1; y < TOTAL_ROWS; y++) {
-				colorArray[x][y] = Color.WHITE;
+
+		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //The rest of the grid
+			for (int y = 0; y < TOTAL_ROWS; y++) {
+				cells[x][y] = Color.WHITE;
 			}
 		}
+		
+		plantMines();
+		//coverGrids();
 	}
+	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-
+		
 		//Compute interior coordinates
 		Insets myInsets = getInsets();
 		int x1 = myInsets.left;
@@ -58,23 +74,26 @@ public class MyPanel extends JPanel {
 		//Draw the grid minus the bottom row (which has only one cell)
 		//By default, the grid will be 10x10 (see above: TOTAL_COLUMNS and TOTAL_ROWS) 
 		g.setColor(Color.BLACK);
-		for (int y = 0; y <= TOTAL_ROWS - 1; y++) {
+		for (int y = 0; y <= TOTAL_ROWS ; y++) {
 			g.drawLine(x1 + GRID_X, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)), x1 + GRID_X + ((INNER_CELL_SIZE + 1) * TOTAL_COLUMNS), y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)));
 		}
 		for (int x = 0; x <= TOTAL_COLUMNS; x++) {
-			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)));
+			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS)));
 		}
 
 		//Draw an additional cell at the bottom left
-		g.drawRect(x1 + GRID_X, y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)), INNER_CELL_SIZE + 1, INNER_CELL_SIZE + 1);
-
-		//Paint cell colors
+		//g.drawRect(x1 + GRID_X, y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS)), INNER_CELL_SIZE + 1, INNER_CELL_SIZE + 1)
+		
+		int mx,my,gx,gy; 
+		
+		//Paint cell colors and evaluate near bombs near the cell
 		for (int x = 0; x < TOTAL_COLUMNS; x++) {
 			for (int y = 0; y < TOTAL_ROWS; y++) {
-				if ((x == 0) || (y != TOTAL_ROWS - 1)) {
-					Color c = colorArray[x][y];
+				if ((x == 0) || (y != TOTAL_ROWS)) {
+					Color c = cells[x][y];
 					g.setColor(c);
 					g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
+					
 				}
 			}
 		}
@@ -99,7 +118,7 @@ public class MyPanel extends JPanel {
 		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
 			return x;
 		}
-		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 1) {   //Outside the rest of the grid
 			return -1;
 		}
 		return x;
@@ -124,9 +143,80 @@ public class MyPanel extends JPanel {
 		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
 			return y;
 		}
-		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 1) {   //Outside the rest of the grid
 			return -1;
 		}
 		return y;
+	}
+	
+	public void plantMines(){
+		for(int i = 0; i < AMOUNT_OF_MINES;i++){
+			generateMines();
+		}
+	}
+	
+	public void generateMines(){
+		random = new Random();
+		int xDirection = random.nextInt(TOTAL_COLUMNS);
+		int yDirection = random.nextInt(TOTAL_ROWS);
+		//TODO 
+		bombs[xDirection][yDirection] = 1;
+		
+	}
+	
+	public void setNumbers(Graphics g){
+		int mx, my, gx, gy;
+		Insets myInsets = getInsets();
+		int x1 = myInsets.left;
+		int y1 = myInsets.top;
+		for(int x = 0; x < TOTAL_COLUMNS;x++){
+			for(int y = 0; y < TOTAL_ROWS; y++){
+				amountOfNearMines = 0;
+				
+				mx = x - 1;
+				gx = x + 1;
+				my = y - 1;
+				gy = y + 1;
+				
+				if(mx >= 0 && my >= 0 && bombs[mx][my] == 1){
+					amountOfNearMines++;
+					neighbors[mx][my] = 2;
+				}
+				if(mx >= 0 && bombs[mx][y] == 1){
+					amountOfNearMines++;
+					neighbors[mx][y] = 2;
+				}
+				if(mx >= 0 && gy < TOTAL_ROWS && bombs[mx][gy] == 1){
+					amountOfNearMines++;
+					neighbors[mx][gy] = 2;
+				}
+				if(gy < TOTAL_ROWS && bombs[x][gy] == 1){
+					amountOfNearMines++;
+					neighbors[x][gy] = 2;
+				}
+				if(gx < TOTAL_COLUMNS && gy < TOTAL_ROWS && bombs[gx][gy] == 1){
+					amountOfNearMines++;
+					neighbors[gx][gy] = 2;
+				}
+				if(gx < TOTAL_COLUMNS && bombs[gx][y] == 1){
+					amountOfNearMines++;
+					neighbors[gx][y] = 2;
+				}
+				if(gx < TOTAL_COLUMNS && my >= 0 && bombs[gx][my] == 1){
+					amountOfNearMines++;
+					neighbors[gx][my] = 2;
+				}
+				if(my >= 0 && bombs[x][my] == 1){
+					amountOfNearMines++;
+					neighbors[x][my] = 2;
+				}
+				
+				if(amountOfNearMines > 0 && bombs[x][y] != 1){
+					g.setColor(Color.BLUE);
+					g.drawString("" + amountOfNearMines, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 12, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) +12);
+					
+				}	
+			}
+		}
 	}
 }
